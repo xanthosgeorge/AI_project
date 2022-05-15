867e8294932403b4b7784d5ca57477080ea7adc6
xanthosgeorge <xanthosgeorge@gmail.com>
2022-05-15 16:28:43 +0300

    Basic Initial Implemantation

---
+++ ai_project.py
@@ -0,0 +1,29 @@
+from random import random
+import random
+
+from typing import List
+list = ["A","B","C","D","E"]
+random.shuffle(list)
+parent1 = random.sample(list,5)
+parent2 = random.sample(list,5)
+
+
+graph = {"A":{"B":4,"C":4,"D":7,"E":3},
+           "B":{("C",2),("D",3),("E",5),("A",4)},
+           "C":{("B",2),("D",2),("E",3),("A",4)},
+           "D":{("B",3),("C",2),("A",7),("E",6)},
+           "E":{("A",3),("B",5),("C",3),("D",6)}
+           }
+print(graph['A']['B'])
+
+#print(parent1,parent2,parent3,parent4)
+
+def CrossOver(parent1,parent2):
+    part1 = parent1[3:]
+    part2 = parent2[3:]
+    parent1 = parent1[:3] + part2
+    parent2 = parent2[:3]+ part1
+    print(part1,part2)
+    print(parent1,parent2)
+
+CrossOver(parent1,parent2)
